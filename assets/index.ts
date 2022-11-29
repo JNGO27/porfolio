102dd8eb4944794e250952def6662b5991976f94
Joseph Ochoa <josephochoa91@gmail.com>
2022-11-29 13:43:48 -0800

    Add technologies I use images

--- assets/index.ts
+++ assets/index.ts
@@ -1,8 +1,29 @@
 import Self from './myself.jpg';
 import Laptop from './laptop.svg';
-
+import TS from './technologies/typescript.png';
+import JS from './technologies/javascript.png';
+import React from './technologies/react.png';
+import HTML from './technologies/html5.png';
+import CSS from './technologies/css.png';
+import StyledComponents from './technologies/styled-components.png';
+import Next from './technologies/next.png';
+import API from './technologies/api.png';
+import Node from './technologies/node.png';
+import SQL from './technologies/sql.png';
+import PostGres from './technologies/postgres.png';
 
 export { 
   Self,
   Laptop,
+  TS,
+  JS,
+  React,
+  HTML,
+  CSS,
+  StyledComponents,
+  Next,
+  API,
+  Node,
+  SQL,
+  PostGres,
 };
