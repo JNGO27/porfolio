7ae704a9d96463d011e603c0d178910bbc22dafb
Joseph Ochoa <josephochoa91@gmail.com>
2022-12-02 09:45:59 -0800

    Fix unescaped characters again

--- components/HeroBanner/HeroBanner.tsx
+++ components/HeroBanner/HeroBanner.tsx
@@ -8,8 +8,8 @@ const HeroBanner = () => {
   return (
     <section className={styles.banner}>
       <div className={styles["banner-content"]}>
-        <h1 className={styles["banner-text"]}>Hi there, I`&apos`m Joseph.<em className={styles["banner-text-gradient"]}> Full Stack Software Engineer.</em> I love building products.</h1>
-        <p>I`&apos`m a Software Engineer who has a deep internal satisfaction through striving to achieve Mastery in my craft and strongly enjoy both Front-End and Back-End development. I have a vision to always get better, never stop learning, and ultimately keep building!</p>
+        <h1 className={styles["banner-text"]}>Hi there, I&apos;m Joseph.<em className={styles["banner-text-gradient"]}> Full Stack Software Engineer.</em> I love building products.</h1>
+        <p>I&apos;m a Software Engineer who has a deep internal satisfaction through striving to achieve Mastery in my craft and strongly enjoy both Front-End and Back-End development. I have a vision to always get better, never stop learning, and ultimately keep building!</p>
       </div>
       <div className={styles["banner-self"]}>
         <div className={styles["icon-wrapper"]}>
