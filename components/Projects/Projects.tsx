c631c32c2eb56ad5ff7b1b9846d12a9530c5fec3
Joseph Ochoa <josephochoa91@gmail.com>
2022-12-13 16:33:14 -0800

    Remove Case Study Number from heading

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
       <h1 className={styles.heading}>Projects</h1>
       {projectsArr.map(project => (
         <div key={project.caseStudy} className={styles["case-study"]}>
-          <h2 className={styles["case-study-heading"]}>Case Study {project.caseStudy} of {projectsArr.length}</h2>
+          <h2 className={styles["case-study-heading"]}>Case Study</h2>
           <Devices project={project} />
         </div>
       ))}
