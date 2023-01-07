f997ab6aeb97c01717e35d89ae5a71ff9a76b222
Joseph Ochoa <josephochoa91@gmail.com>
2023-01-07 13:21:34 -0800

    fix wording and grammar mistakes

--- components/HeroBanner/HeroBanner.tsx
+++ components/HeroBanner/HeroBanner.tsx
@@ -9,7 +9,7 @@ const HeroBanner = () => {
     <section className={styles.banner}>
       <div className={styles["banner-content"]}>
         <h1 className={styles["banner-text"]}>Hi there, I&apos;m Joseph.<em className={styles["banner-text-gradient"]}> Full Stack Software Engineer.</em> I love building products.</h1>
-        <p>I&apos;m a Software Engineer who has a deep internal satisfaction through striving to achieve Mastery in my craft and strongly enjoy both Front-End and Back-End development. I have a vision to always get better, never stop learning, and ultimately keep building!</p>
+        <p>As a software engineer, I find deep internal satisfaction in striving for mastery in my craft. I am passionate about both front-end and back-end development and am driven to continually improve and learn new skills. My goal is to keep building and never stop learning.</p>
       </div>
       <div className={styles["banner-self"]}>
         <div className={styles["icon-wrapper"]}>
